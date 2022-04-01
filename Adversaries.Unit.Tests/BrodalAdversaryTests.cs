a0ff479fb19fea8cb93027276c4bb1193e3bf07d
Nick Nash <nick@waitfree.technology>
2022-04-01 12:14:07 +0100

    Add a placeholder test for Brodal's adversary, perhaps exhuastively test that it produces a partial order for up to some fixed N as a better test?

---
+++ Adversaries.Unit.Tests/BrodalAdversaryTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace AdversaryExperiments.Adversaries.Unit.Tests
+{
+    [TestFixture]
+    public class BrodalAdversaryTests
+    {
+        [Test]
+        public void PlaceHolderAddProperTests()
+        {
+            var adv = new BrodalAdversary(10);
+            var f = adv.Compare(adv.CurrentData[0], adv.CurrentData[1]);
+            var s = adv.Compare(adv.CurrentData[0], adv.CurrentData[1]);
+            var t = adv.Compare(adv.CurrentData[1], adv.CurrentData[0]);
+
+            Assert.That(f, Is.EqualTo(-1));
+            Assert.That(s, Is.EqualTo(-1));
+            Assert.That(t, Is.EqualTo(1));
+        }
+    }
+}
