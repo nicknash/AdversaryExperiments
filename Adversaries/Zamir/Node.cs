1f045875aa850d38340f2310cf091f32bafe95d3
Nick Nash <nicholas.nash@gmail.com>
2022-04-22 07:46:32 +0100

    Add very tentative skeleton of Zamir et al's ternary adversary

---
+++ Adversaries/Zamir/Node.cs
@@ -0,0 +1,46 @@
+﻿namespace AdversaryExperiments.Adversaries.Zamir
+{
+    //       T 
+    //      / \
+    //     I  I
+    //   /  \/ \
+    //   T   T  T
+    // T: tree node
+    // I: intermediate node
+    class Node
+    {
+        public enum NodeType
+        {
+            Sentinel,
+            Tree,
+            Intermediate
+        }
+        
+        public Node Left { get; private set; }
+        public Node Right { get; private set; }
+        
+        public NodeType Type { get; private set; }
+
+        public static Node CreateUnit()
+        {
+            var child0 = CreateFinalLevel();
+            var child1 = CreateFinalLevel();
+            var child2 = CreateFinalLevel();
+
+            var child01 = new Node { Left = child0, Right = child1, Type = NodeType.Intermediate};
+            var child12 = new Node { Left = child1, Right = child2, Type = NodeType.Intermediate};
+
+            var root = new Node
+            {
+                Left = child01,
+                Right = child12
+            };
+
+            return root;
+        }
+
+        private static Node CreateFinalLevel() => new() { Type = NodeType.Tree, Left = CreateSentinel(), Right = CreateSentinel() };
+        private static Node CreateSentinel() => new() { Type = NodeType.Sentinel };
+        
+    }
+}
---
+++ Adversaries/Zamir/ZamirTernaryAdversary.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdversaryExperiments.Adversaries.Zamir
+{
+    public class ZamirTernaryAdversary : IAdversary
+    {
+        private const int Equal = 0;
+        private const int Less = -1;
+        private const int Greater = 1;
+   
+        public string Name { get; }
+        public long NumComparisons { get; private set; }
+        public List<WrappedInt> CurrentData { get; }
+
+        private Node _root;
+
+        public ZamirTernaryAdversary(int size)
+        {
+            Name = "Zamir3";
+            CurrentData = new List<WrappedInt>(Enumerable.Range(0, size).Select(i => new WrappedInt { Value = i }));
+            _root = Node.CreateUnit();
+        }
+        
+        public int Compare(WrappedInt x, WrappedInt y)
+        {
+            ++NumComparisons;
+            if(x == y)
+            {
+                return Equal;
+            }
+
+            var xIsAncestor = ExistsPath(x, y);
+            var yIsAncestor = ExistsPath(y, x);
+
+            var inSameNode = xIsAncestor && yIsAncestor;
+            var requiresClassification = xIsAncestor || yIsAncestor;
+
+            var isXPaired = TryGetPair(x, out var xPair);
+            var isYPaired = TryGetPair(y, out var yPair);
+
+            if(inSameNode)
+            {
+
+            }
+            else if(requiresClassification)
+            {
+                
+                if(!isXPaired && !isYPaired)
+                {
+                    // If there 
+                }
+            }
+            // TODO: The ordering is already defined, return it.
+
+            return -1;
+        }
+
+        private Node GetNode(WrappedInt v) => throw new System.Exception("TODO1");
+
+        private bool TryGetPair(WrappedInt v, out WrappedInt w) => throw new System.Exception("TODO2");
+
+        private bool ExistsPath(WrappedInt x, WrappedInt y) => throw new System.Exception("TODO3");
+    }
+}
