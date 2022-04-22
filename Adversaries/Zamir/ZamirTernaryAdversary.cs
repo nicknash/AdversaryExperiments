8c347900f1164eca97bdb84fe4cf09349dcfc4a7
Nick Nash <nicholas.nash@gmail.com>
2022-04-22 11:34:43 +0100

    Zamir adversary: Sketch of unpaired distinct node element comparisons.

--- Adversaries/Zamir/ZamirTernaryAdversary.cs
+++ Adversaries/Zamir/ZamirTernaryAdversary.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdversaryExperiments.Adversaries.Zamir
@@ -53,6 +54,8 @@ namespace AdversaryExperiments.Adversaries.Zamir
                     if (!isXPaired && !isYPaired)
                     {
                         // These are the 'minimal push down' case of the ancestor
+                        var result = xIsAncestor ? PushDown(x, y) : OtherSense(PushDown(y, x));
+                        return result;
                     }
                     else
                     {
@@ -68,10 +71,57 @@ namespace AdversaryExperiments.Adversaries.Zamir
             }
         }
 
-        private Node GetNode(WrappedInt v) => throw new System.Exception("TODO1");
+        private int PushDown(WrappedInt ancestor, WrappedInt descendant)
+        {
+            if(CanPushLeft(ancestor, descendant))
+            {
+                return Less;
+            }
+            else if(CanPushRight(ancestor, descendant))
+            {
+                return Greater;
+            }
+            else
+            {
+                if(CanPushTwiceLeft(ancestor, descendant))
+                {
+                    return Less;
+                }
+                if(CanPushTwiceRight(ancestor, descendant))
+                {
+                    return Greater;
+                }
+                throw new Exception($"Should never happen: Cannot resolve comparison of two unpaired elements in distinct nodes.");
+            }
+        }
 
-        private bool TryGetPair(WrappedInt v, out WrappedInt w) => throw new System.Exception("TODO2");
+        private int OtherSense(int r)
+         => r switch 
+         {
+             Greater => Less,
+             Less => Greater,
+             _ => throw new Exception($"Unexpected comparison result {r}")
+         };
 
-        private bool ExistsPath(WrappedInt x, WrappedInt y) => throw new System.Exception("TODO3");
+        private Node GetNode(WrappedInt v) => throw new Exception("TODO1");
+
+        private bool TryGetPair(WrappedInt v, out WrappedInt w) => throw new Exception("TODO2");
+
+        private bool ExistsPath(WrappedInt x, WrappedInt y) => throw new Exception("TODO3");
+
+        private bool ExistsPath(Node x, Node y) => throw new Exception($"TODO4");
+ 
+        private bool CanPushLeft(Node x, Node y) => !ExistsPath(x.Left, y) && !ExistsPath(y, x.Left);
+        private bool CanPushRight(Node x, Node y) => throw new Exception($"TODO");
+
+        private bool CanPushLeft(WrappedInt x, WrappedInt y) => CanPushLeft(GetNode(x), GetNode(y));
+
+        private bool CanPushTwiceLeft(WrappedInt x, WrappedInt y) => CanPushLeft(GetNode(x).Left, GetNode(y));
+
+        private bool CanPushRight(WrappedInt x, WrappedInt y) => throw new Exception($"TODO");
+
+        private bool CanPushTwiceRight(WrappedInt x, WrappedInt y) => throw new Exception($"TODO");
+
+
     }
 }
