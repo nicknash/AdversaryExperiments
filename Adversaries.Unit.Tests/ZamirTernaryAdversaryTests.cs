53c80f0cc5a07508c09586d029db3336a3f80f53
Nick Nash <nicholas.nash@gmail.com>
2022-04-24 08:55:12 +0100

    Stub of tests

---
+++ Adversaries.Unit.Tests/ZamirTernaryAdversaryTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace AdversaryExperiments.Adversaries
+{
+    [TestFixture]
+    public class ZamirTernaryAdversaryTests
+    {
+        public void Compare_RootToRightTreeNode_ReturnsLess()
+        {
+            // All tests here will need the same-node case dealt with first
+        }
+    }
+}
