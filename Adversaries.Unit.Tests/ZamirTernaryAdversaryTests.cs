cc14bd7f9bbdf654422c0b7432f4eb8f2783d74e
Nick Nash <nicholas.nash@gmail.com>
2022-04-25 13:03:33 +0100

    Add trivial test

--- Adversaries.Unit.Tests/ZamirTernaryAdversaryTests.cs
+++ Adversaries.Unit.Tests/ZamirTernaryAdversaryTests.cs
@@ -1,3 +1,4 @@
+using AdversaryExperiments.Adversaries.Zamir;
 using NUnit.Framework;
 
 namespace AdversaryExperiments.Adversaries
@@ -5,6 +6,17 @@ namespace AdversaryExperiments.Adversaries
     [TestFixture]
     public class ZamirTernaryAdversaryTests
     {
+
+        [Test]
+        public void Compare_FirstComparisonBothInRoot_ReturnsLess()
+        {
+            var adversary = new ZamirTernaryAdversary(10);
+
+            var result = adversary.Compare(adversary.CurrentData[0], adversary.CurrentData[1]);
+        
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
         public void Compare_RootToRightTreeNode_ReturnsLess()
         {
             // All tests here will need the same-node case dealt with first
