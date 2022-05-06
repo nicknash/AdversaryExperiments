7a8157f9a61a3a65a06491cd3a6e327aa1b8b99a
Nick Nash <nicholas.nash@gmail.com>
2022-05-06 14:26:01 +0100

    Add beginning of counting edges in transitive closure using Italiano's algorithm.

--- Adversaries/Closure/ItalianoDAG.cs
+++ Adversaries/Closure/ItalianoDAG.cs
@@ -8,19 +8,46 @@ namespace AdversaryExperiments.Adversaries.Closure
     // O(n^2) space is required.
     class ItalianoDAG
     {
-        
+        // _index[i, j] = The node for vertex number j in the spanning tree for vertex number i if j is a descendant of i
+        //                otherwise, null
+        private readonly SpanningTreeNode[,] _index; 
+        private readonly int _numVerts;
 
         public ItalianoDAG(int numVerts)
         {
-
+            _index = new SpanningTreeNode[numVerts, numVerts];
+            _numVerts = numVerts;
         }
 
         // Count the number of edges that would be exist in the transitive closure of the DAG 
         // if the edge source -> target was added.
         public int CountClosureEdges(int source, int target)
         {
+            if(ExistsDirectedPath(source, target))
+            {
+                return 0;
+            }
+            for(int x = 0; x < _numVerts; ++x)
+            {
+                // The condition in this 'if' is true if 'x' is a vertex of this type
+                //  
+                // x----->source        target---->v
+                //
+                // That is, the addition of the edge source--->target gives rise to a new path
+                // from x to target (and the descendants of target)
+                if(ExistsDirectedPath(x, source) && !ExistsDirectedPath(x, target))
+                {
+                    Meld(x, target, source, target);
+                }
+            }
             return -1;
         }
+
+        private void Meld(int nodeWithNewDescendants, int newlyReachableNode, int newNodeRoot, int newlyReachableNodeRoot)
+        {
+
+        }
+
 
         // Add the edge source -> target to the DAG, internally updating the transitive closure of the DAG
         public void AddEdge(int source, int target)
@@ -28,9 +55,11 @@ namespace AdversaryExperiments.Adversaries.Closure
 
         }
 
-        public bool ExistsDirectedPath(int source, int target)
-        {
-            throw new System.Exception("TODO");
-        }
+        public bool ExistsDirectedPath(int source, int target) => _index[source, target] != null;
+    }
+
+    class SpanningTreeNode
+    {
+
     }
 }
