e48029ed0a189363f72df2ad3f21c096ff2c82a5
Nick Nash <nicholas.nash@gmail.com>
2022-05-07 11:52:15 +0100

    actually sort out closure adversary

--- Adversaries/Closure/ClosureAdversary.cs
+++ Adversaries/Closure/ClosureAdversary.cs
@@ -1,24 +1,33 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdversaryExperiments.Adversaries.Closure
 {
-    class ClosureAdversary : IAdversary
+    public class ClosureAdversary : IAdversary
     {
-        private ItalianoDAG _dag;
+        private readonly ItalianoDAG _dag;
 
-        public string Name => throw new System.NotImplementedException();
+        public string Name {get; }
 
-        public long NumComparisons => throw new System.NotImplementedException();
+        public long NumComparisons { get; private set; }
 
-        public List<WrappedInt> CurrentData => throw new System.NotImplementedException();
+        public List<WrappedInt> CurrentData { get; }
+
+        public ClosureAdversary(int size)
+        {
+            Name = "Closure";
+            CurrentData = new List<WrappedInt>(Enumerable.Range(0, size).Select(i => new WrappedInt { Value = i }));
+            _dag = new ItalianoDAG(size);
+        }
 
         public int Compare(WrappedInt x, WrappedInt y)
         {
+            ++NumComparisons;
             if(x == y)
             {
                 return 0;
             }
-            
+
             if(_dag.ExistsDirectedPath(x.Value, y.Value))
             {
                 return -1;
@@ -27,14 +36,14 @@ namespace AdversaryExperiments.Adversaries.Closure
             {
                 return 1;
             }
-            if(_dag.CountClosureEdges(x.Value, y.Value) < _dag.CountClosureEdges(x.Value, y.Value))
+            if(_dag.CountClosureEdges(x.Value, y.Value) < _dag.CountClosureEdges(y.Value, x.Value))
             {
                 _dag.AddEdge(x.Value, y.Value);
                 return -1;
             }
             else
             {
-                _dag.AddEdge(x.Value, y.Value);
+                _dag.AddEdge(y.Value, x.Value);
                 return 1;
             }
         }
