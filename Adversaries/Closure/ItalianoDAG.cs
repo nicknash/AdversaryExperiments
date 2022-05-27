320259d8a43259f48b79ca57035615a86d2bd8c4
Nick Nash <nicholas.nash@gmail.com>
2022-05-27 10:53:06 +0100

    Add explanation of 'obvious' amortized time.

--- Adversaries/Closure/ItalianoDAG.cs
+++ Adversaries/Closure/ItalianoDAG.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AdversaryExperiments.Adversaries.Closure
@@ -8,13 +10,55 @@ namespace AdversaryExperiments.Adversaries.Closure
     //
     // For an n vertex graph, adding a new edge takes O(n) amortized time. Querying the existence of a path takes O(1) time.
     // O(n^2) space is required.
-    public class ItalianoDAG
+    //
+    //
+    // Italiano's amortized analysis using a potential function is pretty weird. Mainly because he uses a potential function
+    // that is zero for the empty graph, and otherwise, negative!
+    //
+    // As a result, his amortized bound isn't an upper bound, because the final (positive) contribution of the final value of the potential function needs
+    // to be bounded from above.
+    //
+    // For my sanity, here is a much simpler amortized analysis (more easily done on an iterative version of the algorithm than Italiano's recursive presentation). This is
+    // a direct 'aggregate analysis' with the minute details spelled out:
+    //
+    // Let n be the number of vertices in the DAG. Consider a sequence of k calls to the 'AddEdge' function below. 
+    // Any call to 'ExistsDirectedPath' takes O(1) time. So, in total over this sequence O(nk) time is spent 
+    // making nk calls to 'CopySpanningTree'.
+    //
+    // 'CopySpanningTree' calls 'MeldIterative'. The outer loop of the latter function iterates exactly as many times
+    // as items are added to '_worklist', but, except for the very first item, all items are added by the inner loop of this function.
+    // Thus, the total time spent in inner loop iterations equals the total time spent in outer loop iterations.
+    // Moreover, each outer-loop iteration adds exactly 1 edge to the transitive closure of the DAG. That is, 'CopySpanningTree' takes O(P) time to add
+    // P edges to the transitive closure of the DAG.
+    //
+    // Let T be the total number of edges adding to the transitive closure by the nk calls made to 'CopySpanningTree'.
+    //
+    // Then the total time taken by the sequence of k calls to 'AddEdge' is:
+    //
+    // O(nk) + O(T) 
+    //
+    // Now It must be the case that T = O(min(n^2,k^2)), since adding k edges to the DAG can add at most k(k-1)/2 edges to the transitive closure, and the 
+    // transitive closure can never have more edges than the complete graph on n vertices which has n(n-1)/2 edges.
+    //
+    // And so the total time taken by the sequence of k calls to 'AddEdge' is:
+    //
+    // O(nk) + O(T) = O(nk) + O(min(n^2, k^2))
+    //
+    // And so the amortized cost per call to 'AddEdge' (in excruciating detail) is:
+    //
+    //  1/k * (O(nk) + O(min(n^2,k^2))
+    // = O(n) + O(min(n^2/k, k))
+    // = O(n) + O(n)
+    // = O(n)
+    //
+    public class ItalianoDAG // TODONICK: Write some tests for this.
     {
         // _index[i, j] = The spanning tree node for vertex number j in the spanning tree for vertex number i if j is a descendant of i
         //                otherwise, null
         //
         private readonly SpanningTreeNode[,] _index; 
         private readonly int _numVerts;
+        private readonly Stack<(SpanningTreeNode, int)> _worklist = new();
 
         public ItalianoDAG(int numVerts)
         {
@@ -37,6 +81,10 @@ namespace AdversaryExperiments.Adversaries.Closure
         // This is the same algorithm as 'AddEdge' but it just counts the edges instead.
         public int CountClosureEdges(int source, int target)
         {
+            if (ExistsDirectedPath(target, source))
+            {
+                throw new Exception($"Adding the edge {source} -> {target} would create a cycle");
+            }
             if(ExistsDirectedPath(source, target))
             {
                 return 0;
@@ -55,10 +103,15 @@ namespace AdversaryExperiments.Adversaries.Closure
         // Add the edge source -> target to the DAG, internally updating the transitive closure of the DAG
         public void AddEdge(int source, int target)
         {
+            if (ExistsDirectedPath(target, source))
+            {
+                throw new Exception($"Adding the edge {source} -> {target} would create a cycle");
+            }
             if(ExistsDirectedPath(source, target))
             {
                 return;
             }
+
             for(int x = 0; x < _numVerts; ++x)
             {
                 // The condition in this 'if' is true if 'x' is a vertex of this type
@@ -77,11 +130,11 @@ namespace AdversaryExperiments.Adversaries.Closure
 
         // Copy the sub-spanning-tree of 'newDescendantVertex' rooted at 'immediateAncestorOfNewDescendantVertex' into the spanning-tree of 'vertexWithNewDescendants', but
         // exclude all nodes that are already in the spanning tree of 'vertexWithNewDescendants'
-        private void CopySpanningTree(int vertexWithNewDescendants, int newDescendantVertex, int immediateAncestorOfNewDescendantVertex) 
-           => Meld(vertexWithNewDescendants, 
+        private void CopySpanningTree(int vertexWithNewDescendants, int newDescendantVertex, int immediateAncestorOfNewDescendantVertex)
+            => MeldIterative(vertexWithNewDescendants, newDescendantVertex, immediateAncestorOfNewDescendantVertex);/*Meld(vertexWithNewDescendants, 
                    newDescendantVertex, 
                    GetSpanningTreeNode(vertexWithNewDescendants, immediateAncestorOfNewDescendantVertex),
-                   newDescendantVertex);
+                   newDescendantVertex);*/
 
         private void Meld(int vertexWithNewDescendants, int newDescendantVertex, SpanningTreeNode parent, int newChildVertexNumber)
         {
@@ -100,7 +153,30 @@ namespace AdversaryExperiments.Adversaries.Closure
                 }
             }
         }
-
+        
+        private void MeldIterative(int vertexWithNewDescendants, int newDescendantVertex, int immediateAncestorOfNewDescendantVertex)
+        {
+            var root = GetSpanningTreeNode(vertexWithNewDescendants, immediateAncestorOfNewDescendantVertex);
+            _worklist.Push((root, newDescendantVertex));
+            while (_worklist.Count > 0)
+            {
+                var (parent, newChildVertexNumber) = _worklist.Pop();
+                var newChild = new SpanningTreeNode(newChildVertexNumber);
+                parent.AddChild(newChild);
+                AddDescendant(vertexWithNewDescendants, newChild);
+                var nodeInOtherTree = GetSpanningTreeNode(newDescendantVertex, newChild.VertexNumber);
+                var children = nodeInOtherTree.Children;
+                for (int i = 0; i < children.Count; ++i)
+                {
+                    var child = children[i];
+                    if (!ExistsDirectedPath(vertexWithNewDescendants, child.VertexNumber))
+                    {
+                        _worklist.Push((nodeInOtherTree, child.VertexNumber));
+                    }
+                }
+            }
+        }
+        
         private int CountMeld(int vertexWithNewDescendants, int newDescendantVertex, int newChildVertexNumber)
         {
             var nodeInOtherTree = GetSpanningTreeNode(newDescendantVertex, newChildVertexNumber);
@@ -138,9 +214,7 @@ namespace AdversaryExperiments.Adversaries.Closure
         }
 
         private void AddDescendant(int ancestor, SpanningTreeNode node) => _index[ancestor, node.VertexNumber] = node;
-
-        private SpanningTreeNode GetSpanningTreeRoot(int vertexNumber) => _index[vertexNumber, vertexNumber];
-
+        
         private SpanningTreeNode GetSpanningTreeNode(int rootVertexNumber, int descendantVertexNumber) => _index[rootVertexNumber, descendantVertexNumber];
     }
 }
